10b37f0f978db84861d99bd62fd414758f320910
Stephan Lensky <public@slensky.com>
2023-03-14 00:55:40 -0400

    Update DM message to reflect leaving testing

--- moobot/constants.py
+++ moobot/constants.py
@@ -1,10 +1,7 @@
 GOOGLE_CALENDAR_SYNC_ENABLE_DM_TEMPLATE = (
-    "**Mooooo!** :cow:\n\nTo finish setting up Google Calendar sync, you will need complete the"
-    " following steps:\n\n1. Ask Stephan to add your email to the list of test users for the Moobot"
-    " Google app. Officially publishing the app (which would allow skipping this step) requires a"
-    " privacy policy and some other legal stuff that is a pain to deal with. Yick.\n2. Authorize"
+    "**Mooooo!** :cow:\n\nTo finish setting up Google Calendar sync, authorize"
     " access to your Google account by clicking the following link:\n{auth_url}\n\nAfter"
-    " authorizing access, new RSVPs to will be automatically synced to your Google Calendar."
+    " authorizing access, your RSVPs to will be automatically synced to your Google Calendar."
 )
 GOOGLE_CALENDAR_SYNC_ENABLE_USER_EXISTS_DM = (
     "Hm... you added a reaction to enable Google Calendar syncing, but sync was already enabled! I"
